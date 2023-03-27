706e4e28951c2e1e16f24f824418000237b5fde6
Jatin-Gupta1 <122719617+Jatin-Gupta1@users.noreply.github.com>
2023-03-27 22:09:32 +0530

    Add files via upload

---
+++ Operators4.java
@@ -0,0 +1,10 @@
+public class Operators4{
+    public static void main(String[] args) {
+        int a=55;
+        int b=70;
+        if(a<50&&a<b)
+            System.out.println("condition 1 is true");
+        if(a<50||a<b)
+            System.out.println("condition 2 is true");
+    }
+}
---
+++ operators1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class operators1 {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            System.out.print((((x + 8) / 3) % 5) * 5);
+        }
+    }
+
